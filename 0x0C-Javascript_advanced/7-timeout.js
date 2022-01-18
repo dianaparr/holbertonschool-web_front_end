f0b7f443e1f269bede273d04349cb49b6c44ccd4
dianaparr <dianaparra.9611@gmail.com>
2022-01-18 14:13:33 -0500

    change settimeout in one line

--- 0x0C-Javascript_advanced/7-timeout.js
+++ 0x0C-Javascript_advanced/7-timeout.js
@@ -1,8 +1,6 @@
 console.log("Start of the execution queue");
 
-setTimeout(() => {
-    console.log("Final code block to be executed");
-}, 0);
+setTimeout(() => {console.log("Final code block to be executed");}, 0);
 
 for (let index = 1; index <= 100; index++) {
     console.log(index);
